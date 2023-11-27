bd2df750f57639e2afce335bd162c7463130e046
jorgevds <jorgevandesompel@gmail.com>
2023-11-27 20:12:05 +0100

    Navbar components

---
+++ components/navbar/footer.tsx
@@ -0,0 +1,13 @@
+export const Footer = () => (
+    <footer className="bg-slate-950 text-white h-full border-t-2 border-red-500">
+        <p>Velo</p>
+        <ul>
+            <li>
+                List of Stations
+            </li>
+            <li>
+                Find station
+            </li>
+        </ul>
+    </footer>
+)
---
+++ components/navbar/header.tsx
@@ -0,0 +1,15 @@
+export const Header = () => (
+    <header className="border-b-2 border-red-500">
+        <nav className="flex items-center w-4/5 h-16 m-auto ">
+            <h1 className="text-xl font-normal">Velo</h1>
+            <ul className="flex ml-auto">
+                <li className="px-2">
+                    Stations
+                </li>
+                <li className="px-2">
+                    Find station
+                </li>
+            </ul>
+        </nav>
+    </header>
+) 
