1cbb4a5160ecd39cff3e05036407b863169957db
jorgevds <jorgevandesompel@gmail.com>
2023-11-29 23:02:24 +0100

    Style header and footer

--- components/navbar/footer.tsx
+++ components/navbar/footer.tsx
@@ -1,13 +1,11 @@
+import Link from "next/link";
+
 export const Footer = () => (
-    <footer className="bg-slate-950 text-white h-full border-t-2 border-red-500">
-        <p>Velo</p>
-        <ul>
-            <li>
-                List of Stations
-            </li>
-            <li>
-                Find station
-            </li>
-        </ul>
+    <footer className="bg-slate-950 text-white flex-grow border-t-2 border-red-500">
+        <article className="flex w-4/5 m-auto">
+            <Link href={"/"}>
+                <p className="text-2xl font-normal pt-12">Velo</p>
+            </Link>
+        </article>
     </footer>
-)
+);
--- components/navbar/header.tsx
+++ components/navbar/header.tsx
@@ -1,15 +1,11 @@
+import Link from "next/link";
+
 export const Header = () => (
     <header className="border-b-2 border-red-500">
         <nav className="flex items-center w-4/5 h-16 m-auto ">
-            <h1 className="text-xl font-normal">Velo</h1>
-            <ul className="flex ml-auto">
-                <li className="px-2">
-                    Stations
-                </li>
-                <li className="px-2">
-                    Find station
-                </li>
-            </ul>
+            <Link href={"/"}>
+                <h1 className="text-4xl font-normal">Velo</h1>
+            </Link>
         </nav>
     </header>
-) 
+);
