7a605f021de560dcb7896384c0b844ee498f298e
jorgevds <jorgevandesompel@gmail.com>
2023-11-29 23:02:38 +0100

    Style hero loading and error components

--- components/hero/hero-error.tsx
+++ components/hero/hero-error.tsx
@@ -1,6 +1,6 @@
 export const HeroError = () => (
-    <>
+    <article className="h-96 text-2xl font-normal py-8 w-full bg-amber-100 text-center">
         Something went wrong loading bicycle stations. Please refresh to try
         again.
-    </>
+    </article>
 );
--- components/hero/hero-loading.tsx
+++ components/hero/hero-loading.tsx
@@ -1,5 +1,5 @@
 export const HeroLoading = () => (
-    <article className="h-4/5 w-full bg-amber-100 text-center">
+    <article className="h-96 text-2xl font-normal py-8 w-full bg-amber-100 text-center">
         Loading...
     </article>
 );
