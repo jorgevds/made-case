facec607c9a9940d82ca7345ccc12375f866b4af
jorgevds <jorgevandesompel@gmail.com>
2023-11-29 23:00:33 +0100

    Move code out of bike map, back into hero

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -1,48 +1,81 @@
 "use client";
-import { BicycleStation, useAntwerpQuery } from "@/store";
+import {
+    defineMarkers,
+    stationToListItem,
+    useAntwerpQuery,
+} from "@/store/index";
 import { MapHeight } from "../map/map.entity";
-import { BikeMapMarker } from "../map/bike/bike-map.entity";
 import { HeroLoading } from "./hero-loading";
 import { HeroError } from "./hero-error";
 import { BikeStationMap } from "../map/bike/bike-map";
+import { useState } from "react";
+import { ListItem } from "../list/list.entity";
+import { useDispatch } from "react-redux";
+import { List } from "../list/list";
+import { Button } from "../atoms/button";
 
-export type DefineMarkersSignature = (
-    stations: BicycleStation[] | undefined
-) => BikeMapMarker[];
-
-export const defineMarkers: DefineMarkersSignature = (
-    stations?: BicycleStation[]
-) =>
-    stations?.map((station) => ({
-        position: [station.latitude, station.longitude],
-        popup: {
-            title: `${station.extra.uid} - ${station.extra.address}`,
-            free: station.free_bikes,
-            empty: station.empty_slots,
-            id: station.id,
-        },
-    })) ?? [];
+const minutesByN = (minutes: number) => 1000 * 60 * minutes;
 
 export const Hero = () => {
-    const { data, isLoading, error } = useAntwerpQuery();
+    const [listVisible, setListVisible] = useState<boolean>(false);
+
+    const { data, isLoading, error } = useAntwerpQuery(undefined, {
+        pollingInterval: minutesByN(5),
+    });
+    const dispatch = useDispatch();
+
     const mapSizing = { size: MapHeight.HALF, opts: { pixels: "" } };
 
     const markers = defineMarkers(data?.network.stations);
 
+    const listItems: ListItem[] =
+        data?.network.stations
+            ?.slice()
+            .sort((a, b) => a.extra.uid - b.extra.uid)
+            .map((station) =>
+                stationToListItem(
+                    station,
+                    dispatch,
+                    markers.find((marker) => marker.popup.id === station.id)
+                )
+            ) ?? [];
+
     return (
-        <article className="h-4/5 w-full bg-amber-100 text-center">
-            <section className="flex flex-col m-auto w-4/5">
+        <article className="h-full w-full bg-amber-100 text-center">
+            <section className="flex flex-col m-auto sm:w-4/5 w-11/12">
                 {error ? (
                     <HeroError />
                 ) : isLoading ? (
                     <HeroLoading />
                 ) : (
                     <>
-                        <p className="text-lg font-normal py-8 ">
+                        <p className="text-2xl font-normal py-8">
                             Looking for a <i>velo</i>? Look no further; find one
                             now!
                         </p>
                         <BikeStationMap markers={markers} sizing={mapSizing} />
+
+                        <div className="flex m-auto my-4">
+                            <Button
+                                style="primary"
+                                onClick={() => setListVisible(!listVisible)}
+                            >
+                                {listVisible
+                                    ? "Hide all bicycle stations"
+                                    : "List all bicycle stations"}
+                            </Button>
+                        </div>
+
+                        <p className="py-8">
+                            Total active stations:{" "}
+                            {data?.network.stations.length}
+                        </p>
+
+                        {listVisible ? (
+                            <div className="contents" data-testid="bike-list">
+                                <List items={listItems} />
+                            </div>
+                        ) : null}
                     </>
                 )}
             </section>
--- components/map/bike/bike-map.tsx
+++ components/map/bike/bike-map.tsx
@@ -1,9 +1,7 @@
 "use client";
 import { Map, MapProps } from "../map";
 
-import { Dispatch, useState } from "react";
-import { List } from "../../list/list";
-import { LayersControl, Marker, Popup } from "react-leaflet";
+import { Marker, Popup } from "react-leaflet";
 
 import {
     BikeMapMarker,
@@ -11,24 +9,7 @@ import {
     FreeBikeStationIcon,
 } from "./bike-map.entity";
 import { BikeMapLegend } from "./bike-map-legend";
-import { BicycleStation, setActiveBikeMarker, useAntwerpQuery } from "@/store";
-import { ListItem } from "@/components/list/list.entity";
-import { useDispatch } from "react-redux";
-import { AnyAction } from "@reduxjs/toolkit";
-
-export const stationToListItem: (
-    station: BicycleStation,
-    dispatch: Dispatch<AnyAction>,
-    marker: BikeMapMarker | undefined
-) => ListItem = (
-    station: BicycleStation,
-    dispatch: Dispatch<AnyAction>,
-    marker: BikeMapMarker | undefined
-) => ({
-    label: `${station.extra.uid} - ${station.extra.address}`,
-    latLng: [station.latitude, station.longitude],
-    onClick: () => dispatch(setActiveBikeMarker(marker)),
-});
+import { BikeMapSearch } from "./bike-map-search";
 
 export interface BikeStationMapProps<
     T extends Pick<BikeMapMarker, "position">[] = [{ position: [0, 0] }],
@@ -40,24 +21,9 @@ export const BikeStationMap: React.FC<BikeStationMapProps<BikeMapMarker[]>> = ({
     markers,
     sizing,
 }) => {
-    const [listVisible, setListVisible] = useState<boolean>(false);
-    const { data } = useAntwerpQuery();
-    const dispatch = useDispatch();
-
-    const listItems: ListItem[] =
-        data?.network.stations
-            ?.slice()
-            .sort((a, b) => a.extra.uid - b.extra.uid)
-            .map((station) =>
-                stationToListItem(
-                    station,
-                    dispatch,
-                    markers.find((marker) => marker.popup.id === station.id)
-                )
-            ) ?? [];
-
     return (
         <>
+            <BikeMapSearch />
             <BikeMapLegend />
             <Map sizing={sizing}>
                 {markers.length > 0
@@ -94,18 +60,6 @@ export const BikeStationMap: React.FC<BikeStationMapProps<BikeMapMarker[]>> = ({
                       )
                     : null}
             </Map>
-            <div className="flex m-auto my-4">
-                <button
-                    onClick={() => setListVisible(!listVisible)}
-                    className="bg-red-500 text-white px-4 py-2"
-                >
-                    List all the bicycle stations
-                </button>
-            </div>
-            <p className="py-8">
-                Total active stations: {data?.network.stations.length}
-            </p>
-            {listVisible ? <List items={listItems} /> : null}
         </>
     );
 };
